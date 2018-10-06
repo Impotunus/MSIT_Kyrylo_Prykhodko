0a4c3012d109a3b8ad4324fd40c15ef779a8d6af
Impotunus <k.g.prihodko@gmail.com>
2018-10-06 17:33:08 +0300

    [LAB1] Done

---
+++ src/bookTrading/ontology/Sell.java
@@ -0,0 +1,17 @@
+/** Class associated to the SELL schema
+ **/
+package bookTrading.ontology;
+
+import jade.content.AgentAction;
+
+public class Sell implements AgentAction {
+    private Book item;
+
+    public Book getItem() {
+        return item;
+    }
+
+    public void setItem(Book item) {
+        this.item = item;
+    }
+}
